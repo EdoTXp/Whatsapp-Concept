cedfeac5e3f79667eb50941c63208a33585ab7b1
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2025-04-24 18:14:12 -0300

    * Add ProfileActivity layout with toolbar, profile image, name input, and update button; * Add profile image; * Add gallery icon; * Add parent activity for ProfileActivity in Manifest; * Add string values for ProfileActivity; * Add circular image style.

--- app/src/main/java/com/deiovannagroup/whatsapp_concept/views/activities/ProfileActivity.kt
+++ app/src/main/java/com/deiovannagroup/whatsapp_concept/views/activities/ProfileActivity.kt
@@ -6,15 +6,40 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.deiovannagroup.whatsapp_concept.R
+import com.deiovannagroup.whatsapp_concept.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityProfileBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setEdgeToEdgeLayout()
+
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        val toolbar = binding.includeToolbarProfile.tbMain
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = getString(R.string.edit_profile)
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    private fun setEdgeToEdgeLayout() {
         enableEdgeToEdge()
-        setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom,
+            )
             insets
         }
     }
